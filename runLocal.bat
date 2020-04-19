22cee6ee17d2ce6ffc4dc73722011ec3be684963
evozong <evozong@gmail.com>
2020-04-19 12:49:21 -0400

    Make local system able to render drafts properly

--- runLocal.bat
+++ runLocal.bat
@@ -1,1 +1,9 @@
-bundle exec jekyll serve
+@echo off
+
+IF "%1"=="" (
+  bundle exec jekyll serve
+) ELSE IF "%1"=="--drafts" (
+  bundle exec jekyll serve --drafts
+) ELSE (
+  ECHO "runlocal.bat [--help|--drafts]"
+)
